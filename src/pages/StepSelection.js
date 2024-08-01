778f5eac0cfcb2214cc19e0fa18bbed7c68050f6
NoTaste-def <ddrnjs1245@gmail.com>
2024-08-02 02:57:44 +0900

    CORS error struggle8

--- src/pages/StepSelection.js
+++ src/pages/StepSelection.js
@@ -61,7 +61,11 @@ const StepSelection = ({ selec, setSelec }) => {
 
   const handleSubmit = async () => {
     try {
-      await axios.post(`${URL}/save-user-todo/`, { user_todo: selec });
+      await axios.get(
+        `${URL}/read-user-todo/`,
+        { user_todo: selec },
+        { withCredentials: true }
+      );
       console.log("Data saved successfully.");
     } catch (error) {
       console.error("Error saving data:", error);
